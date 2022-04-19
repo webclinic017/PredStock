3c127c9692c2dce009bc254e42111e7fc7809bbc
T.Miya <toshiki.miyajima@gmail.com>
2022-04-19 14:28:50 +0900

    oc3rの下限を0とする

--- script/1z_FLJOIN.py
+++ script/1z_FLJOIN.py
@@ -70,7 +70,7 @@ def crate(df, uname):
     return df
 
 def clip(df, uname):
-    df[uname] = df[uname].clip(lower = -0.5, upper = 0.8)
+    df[uname] = df[uname].clip(lower = 0, upper = 0.8)
     return df
 
 # %%
